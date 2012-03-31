ba4fa651c63290db030de34e1d7685ed76c732c5
Piotr Pociask <ppociask@o2.pl>
2012-03-31 20:39:31 +0200

    remove icon from Plugin menu and toolbar remove unnecessary files and code lines

--- qnote.py
+++ qnote.py
@@ -20,94 +20,47 @@
  ***************************************************************************/
 """
 # Import the PyQt and QGIS libraries
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt4.QtCore import QFileInfo, QSettings, QCoreApplication, QObject, SIGNAL, Qt
+from qgis.core import QgsApplication, QgsProject
+
 from PyQt4 import uic
-from qgis.core import *
-# Initialize Qt resources from file resources.py
-import resources
-import os
-# Import the code for the dialog
-#from qnotedialog import qNoteDialog
+from os import path
 
 class qNote:
 
     def __init__(self, iface):
         # Save reference to the QGIS interface
-        self.iface = iface
-        # Create the dialog and keep reference
-        #self.dlg = qNoteDialog()
-        # initialize plugin directory
-        self.plugin_dir = QFileInfo(QgsApplication.qgisUserDbFilePath()).path() + "/python/plugins/qnote"
-        # initialize locale
-        localePath = ""
-        locale = QSettings().value("locale/userLocale").toString()[0:2]
-       
-        if QFileInfo(self.plugin_dir).exists():
-            localePath = self.plugin_dir + "/i18n/qnote_" + locale + ".qm"
-
-        if QFileInfo(localePath).exists():
-            self.translator = QTranslator()
-            self.translator.load(localePath)
-
-            if qVersion() > '4.3.3':
-                QCoreApplication.installTranslator(self.translator)        
+        self.iface = iface       
     
     def initGui(self):
-        # Create action that will start plugin configuration
-        self.action = QAction(QIcon(":/plugins/qnote/icon.png"), u"qNote", self.iface.mainWindow())
-        # connect the action to the run method
-        QObject.connect(self.action, SIGNAL("triggered()"), self.run)
-        
-
         proj = QgsProject.instance()
         QObject.connect(proj, SIGNAL("readProject(const QDomDocument &)"),self.loadData)
         QObject.connect(proj, SIGNAL("writeProject(QDomDocument &)"),self.saveData)
         QObject.connect(self.iface, SIGNAL("newProjectCreated ()"),self.clearEdit)
-
-        # Add toolbar button and menu item
-        self.iface.addToolBarIcon(self.action)
-        self.iface.addPluginToMenu(u"&qNote", self.action)
         
-        path = os.path.dirname(os.path.abspath(__file__))
-        self.dock = uic.loadUi(os.path.join(path, "ui_qnote.ui"))
+        pluginPath = path.dirname(path.abspath(__file__))
+        self.dock = uic.loadUi(path.join(pluginPath, "ui_qnote.ui"))
         self.iface.addDockWidget(Qt.BottomDockWidgetArea, self.dock)
         self.loadData()
-        
 
     def unload(self):
-        # Remove the plugin menu item and icon
-        self.iface.removePluginMenu(u"&qNote",self.action)
-        self.iface.removeToolBarIcon(self.action)
         self.iface.removeDockWidget(self.dock)
 
-    # run method that performs all the real work
     def run(self):
         self.iface.addDockWidget(Qt.BottomDockWidgetArea, self.dock)
         self.loadData()
-        """
-        # show the dialog
-        self.dlg.show()
-        # Run the dialog event loop
-        result = self.dlg.exec_()
-        # See if OK was pressed
-        if result == 1:
-            # do something useful (delete the line containing pass and
-            # substitute with your code)
-            pass"""
             
     def saveData(self):
         proj = QgsProject.instance()
         text = self.dock.edit.toPlainText()
         if text:
-            proj.writeEntry('qnote', 'text', text)
+            proj.writeEntry('qnote', 'data', text)
         else:
-            proj.removeEntry('qnote', 'text')
-
+            proj.removeEntry('qnote', 'data')
     
     def loadData(self):
         proj = QgsProject.instance()
-        text = proj.readEntry('qnote', 'text', '')[0]
+        text = proj.readEntry('qnote', 'data', '')[0]
         self.dock.edit.setText( text )
     
     def clearEdit(self):

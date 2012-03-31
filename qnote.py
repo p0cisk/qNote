83b6692e802791fcbb3cf491b2a1ac34275b2dae
Piotr Pociask <ppociask@o2.pl>
2012-03-31 19:26:27 +0200

    docked window save/read text from project file

--- qnote.py
+++ qnote.py
@@ -22,11 +22,13 @@
 # Import the PyQt and QGIS libraries
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from PyQt4 import uic
 from qgis.core import *
 # Initialize Qt resources from file resources.py
-import resources_rc
+import resources
+import os
 # Import the code for the dialog
-from qnotedialog import qNoteDialog
+#from qnotedialog import qNoteDialog
 
 class qNote:
 
@@ -34,7 +36,7 @@ class qNote:
         # Save reference to the QGIS interface
         self.iface = iface
         # Create the dialog and keep reference
-        self.dlg = qNoteDialog()
+        #self.dlg = qNoteDialog()
         # initialize plugin directory
         self.plugin_dir = QFileInfo(QgsApplication.qgisUserDbFilePath()).path() + "/python/plugins/qnote"
         # initialize locale
@@ -49,27 +51,40 @@ class qNote:
             self.translator.load(localePath)
 
             if qVersion() > '4.3.3':
-                QCoreApplication.installTranslator(self.translator)
-   
-
+                QCoreApplication.installTranslator(self.translator)        
+    
     def initGui(self):
         # Create action that will start plugin configuration
-        self.action = QAction(QIcon(":/plugins/qnote/icon.png"), \
-            u"qNote", self.iface.mainWindow())
+        self.action = QAction(QIcon(":/plugins/qnote/icon.png"), u"qNote", self.iface.mainWindow())
         # connect the action to the run method
         QObject.connect(self.action, SIGNAL("triggered()"), self.run)
+        
+
+        proj = QgsProject.instance()
+        QObject.connect(proj, SIGNAL("readProject(const QDomDocument &)"),self.loadData)
+        QObject.connect(proj, SIGNAL("writeProject(QDomDocument &)"),self.saveData)
+        QObject.connect(self.iface, SIGNAL("newProjectCreated ()"),self.clearEdit)
 
         # Add toolbar button and menu item
         self.iface.addToolBarIcon(self.action)
         self.iface.addPluginToMenu(u"&qNote", self.action)
+        
+        path = os.path.dirname(os.path.abspath(__file__))
+        self.dock = uic.loadUi(os.path.join(path, "ui_qnote.ui"))
+        self.iface.addDockWidget(Qt.BottomDockWidgetArea, self.dock)
+        
 
     def unload(self):
         # Remove the plugin menu item and icon
         self.iface.removePluginMenu(u"&qNote",self.action)
         self.iface.removeToolBarIcon(self.action)
+        self.iface.removeDockWidget(self.dock)
 
     # run method that performs all the real work
     def run(self):
+        self.iface.addDockWidget(Qt.BottomDockWidgetArea, self.dock)
+        self.loadData()
+        """
         # show the dialog
         self.dlg.show()
         # Run the dialog event loop
@@ -78,4 +93,21 @@ class qNote:
         if result == 1:
             # do something useful (delete the line containing pass and
             # substitute with your code)
-            pass
+            pass"""
+            
+    def saveData(self):
+        proj = QgsProject.instance()
+        text = self.dock.edit.toPlainText()
+        if text:
+            proj.writeEntry('qnote', 'text', text)
+        else:
+            proj.removeEntry('qnote', 'text')
+
+    
+    def loadData(self):
+        proj = QgsProject.instance()
+        text = proj.readEntry('qnote', 'text', '')[0]
+        self.dock.edit.setText( text )
+    
+    def clearEdit(self):
+        self.dock.edit.setText('')
